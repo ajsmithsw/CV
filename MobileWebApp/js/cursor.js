855309824068247748ea6b6f02d632df9ed1bc00
Alex <ajsmithsw@gmail.com>
2015-10-27 22:33:52 +0000

    Simple home page created.
    Website is live!

---
+++ MobileWebApp/js/cursor.js
@@ -0,0 +1,15 @@
+/**
+ * @author Alexander Smith
+ */
+
+$(document).ready(function() {
+    setInterval ('cursorAnimation()', 600);
+});
+
+function cursorAnimation() {
+    $('#cursor').animate({
+        opacity: 0
+    }, 'fast', 'swing').animate({
+        opacity: 1
+    }, 'fast', 'swing');
+}
