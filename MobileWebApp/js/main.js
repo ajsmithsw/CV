90bdc28dbb61caee4945492d2e45f07718e572cd
Alex <ajsmithsw@gmail.com>
2015-10-28 22:23:53 +0000

    General updates

---
+++ MobileWebApp/js/main.js
@@ -0,0 +1,13 @@
+/**
+ * @author Alexander Smith
+ */
+
+$(document).ready(function() {
+	if (window.navigator.standalone) {
+    document.getElementById("addToHomePrompt").style="visibility:hidden";
+    
+    //style.visibility="hidden";
+
+	}
+    
+});
